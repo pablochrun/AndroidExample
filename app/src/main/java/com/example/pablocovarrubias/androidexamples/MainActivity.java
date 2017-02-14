47e192913157cef14e18228488f844a6245f6e87
Pablo Covarrubias Herrera <pablo.covarrubias@altran.es>
2017-02-14 16:52:54 +0100

    Update with fragments, non usefull

--- app/src/main/java/com/example/pablocovarrubias/androidexamples/MainActivity.java
+++ app/src/main/java/com/example/pablocovarrubias/androidexamples/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.pablocovarrubias.androidexamples;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+//public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     public final static String EXTRA_MESSAGE = "com.example.pablocovarrubias.androidexamples.MESSAGE";
 
@@ -14,14 +19,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 }
